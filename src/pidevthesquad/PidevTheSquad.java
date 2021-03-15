b971d1647cf831468044029dd9d2b88a67acb24d
Gickel OKABI <=>
2021-03-15 14:18:50 +0100

    first test

---
+++ src/pidevthesquad/PidevTheSquad.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pidevthesquad;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author gicke
+ */
+public class PidevTheSquad extends Application{
+
+  
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/Vue/FXMLTeste.fxml"));
+        Scene s = new Scene(root);
+        stage.setScene(s);
+        stage.show();
+    }
+    
+      /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        launch(args);
+
+    }
+    
+    
+}
